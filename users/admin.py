ebbed2cf890e8edd19ac94e3a6dbdf19677050e1
gi-ong <pugo9165@gmail.com>
2021-12-07 20:21:08 +0900

    master

---
+++ users/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    # 위 내용이랑 admin.site.register(models.User, CustomUserAdmin) 도 같은 내용
+    """Custim User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Banana",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+    list_filter = UserAdmin.list_filter + ("superhost",)
+
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )
