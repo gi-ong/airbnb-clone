4eb09daa452519e238f88e3ee11d352d1887f969
gi-ong <pugo9165@gmail.com>
2021-12-10 16:41:59 +0900

    "seed_list"

--- lists/management/commands/seed_list.py
+++ lists/management/commands/seed_list.py
@@ -2,21 +2,23 @@ import random
 from django.core.management.base import BaseCommand
 from django.contrib.admin.utils import flatten
 from django_seed import Seed
-from reviews import models as review_models
+from lists import models as list_models
 from users import models as user_models
 from rooms import models as room_models
+
+NAME = "lists"
 
 
 class Command(BaseCommand):
 
-    help = "This command creates lists"
+    help = f"This command creates {NAME}"
 
     def add_arguments(self, parser):
         parser.add_argument(
             "--number",
             default=2,
             type=int,
-            help="How many lists you want to create",
+            help=f"How many {NAME} you want to create",
         )
 
     def handle(self, *args, **options):
@@ -25,23 +27,12 @@ class Command(BaseCommand):
         users = user_models.User.objects.all()
         rooms = room_models.Room.objects.all()
         seeder.add_entity(
-            review_models.Review,
-            number,
-            {
-                "accuracy": lambda x: random.randint(0, 6),
-                "communication": lambda x: random.randint(0, 6),
-                "cleanliness": lambda x: random.randint(0, 6),
-                "location": lambda x: random.randint(0, 6),
-                "check_in": lambda x: random.randint(0, 6),
-                "value": lambda x: random.randint(0, 6),
-                "room": lambda x: random.choice(rooms),
-                "user": lambda x: random.choice(users),
-            },
+            list_models.List, number, {"user": lambda x: random.choice(users)}
         )
         created = seeder.execute()
         cleaned = flatten(list(created.values()))
         for pk in cleaned:
             list_model = list_models.List.objects.get(pk=pk)
-            to_add = rooms[random.randint(0, 5), random.randint(6, 30)]
+            to_add = rooms[random.randint(0, 5) : random.randint(6, 30)]
             list_model.rooms.add(*to_add)
-        self.stdout.write(self.style.SUCCESS(f"{number} lists created!"))
+        self.stdout.write(self.style.SUCCESS(f"{number} {NAME} created!"))
