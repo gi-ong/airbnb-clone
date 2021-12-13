fc172b9a550276837fbdbce5df84fe35bb9373a7
gi-ong <pugo9165@gmail.com>
2021-12-13 23:34:42 +0900

    "page config#2-2"

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,18 @@
-from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
+from django.core.paginator import EmptyPage, Paginator
 from . import models
 
 
 def all_rooms(request):
-    page = request.GET.get("page")
+    page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(
-        request,
-        "rooms/home.html",
-        {"rooms": rooms},
-    )
+    paginator = Paginator(room_list, 10, orphans=4)
+    try:
+        rooms = paginator.page(int(page))
+        return render(
+            request,
+            "rooms/home.html",
+            {"page": rooms},
+        )
+    except EmptyPage:
+        return redirect("/")
