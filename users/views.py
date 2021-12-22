7982c76e7e47e5f2f8d0fa6bb9462039fbcb4c4d
gi-ong <pugo9165@gmail.com>
2021-12-23 00:39:43 +0900

    "#18.3"

--- users/views.py
+++ users/views.py
@@ -31,6 +31,7 @@ def log_out(request):
 class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
+    # form_class = UserCreationForm
     success_url = reverse_lazy("core:home")
     initial = {
         "first_name": "omg",
