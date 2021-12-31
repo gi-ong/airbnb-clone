9dfd027f281ff2410c703a11232b13e2afb01b21
gi-ong <pugo9165@gmail.com>
2022-01-01 00:04:14 +0900

    "#24.14"

--- reservations/views.py
+++ reservations/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from django.shortcuts import render, redirect, reverse
 from rooms import models as room_models
 from django.contrib.auth.decorators import login_required
+from reviews import forms as review_forms
 from . import models
 
 
@@ -43,8 +44,11 @@ class ReservationDetailView(View):
             and reservation.room.host != self.request.user
         ):
             raise Http404()
+        form = review_forms.CreateReviewForm()
         return render(
-            self.request, "reservations/detail.html", {"reservation": reservation}
+            self.request,
+            "reservations/detail.html",
+            {"reservation": reservation, "form": form},
         )
 
 
