42ad3cbe496e22403094475175036982c5f7e941
gi-ong <pugo9165@gmail.com>
2022-01-03 15:41:42 +0900

    "#25.3"

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -17,14 +18,17 @@ class User(AbstractUser):
     GENDER_OTHER = "other"
 
     GENDER_CHOICES = (
-        (GENDER_MALE, "Male"),
-        (GENDER_FEMALE, "Female"),
-        (GENDER_OTHER, "Other"),
+        (GENDER_MALE, _("Male")),
+        (GENDER_FEMALE, _("Female")),
+        (GENDER_OTHER, _("Other")),
     )
     LANGUAGE_ENGLISH = "en"
     LANGUAGE_KOREAN = "kr"
 
-    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, _("English")),
+        (LANGUAGE_KOREAN, _("Korean")),
+    )
 
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
@@ -45,28 +49,35 @@ class User(AbstractUser):
     # )
 
     avatar = models.ImageField(
+        _("avatar"),
         blank=True,
         upload_to="avatars",
     )
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
-    bio = models.TextField(blank=True)
-    birthdate = models.DateField(blank=True, null=True)
+    gender = models.CharField(
+        _("gender"), choices=GENDER_CHOICES, max_length=10, blank=True
+    )
+    bio = models.TextField(_("bio"), blank=True)
+    birthdate = models.DateField(_("birthdate"), blank=True, null=True)
     language = models.CharField(
+        _("language"),
         choices=LANGUAGE_CHOICES,
         max_length=2,
         blank=True,
         default=LANGUAGE_KOREAN,
     )
     currency = models.CharField(
+        _("currency"),
         choices=CURRENCY_CHOICES,
         max_length=3,
         null=True,
         blank=True,
         default=CURRENCY_KRW,
     )
-    superhost = models.BooleanField(default=False)
-    email_verified = models.BooleanField(default=False)
-    email_secret = models.CharField(max_length=20, default="", blank=True)
+    superhost = models.BooleanField(_("superhost"), default=False)
+    email_verified = models.BooleanField(_("email_verified"), default=False)
+    email_secret = models.CharField(
+        _("email_secret"), max_length=20, default="", blank=True
+    )
     login_method = models.CharField(
         max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL
     )
@@ -82,7 +93,7 @@ class User(AbstractUser):
                 "emails/verify_email.html", {"secret": secret}
             )
             send_mail(
-                "Verify Airbnb Account",
+                _("Verify Airbnb Account"),
                 strip_tags(html_message),
                 settings.EMAIL_FROM,
                 [self.email],
