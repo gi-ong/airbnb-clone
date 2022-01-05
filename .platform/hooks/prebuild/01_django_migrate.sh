8dbfd77a0f24754439bc70dedcc3c5dd8e2d88dc
gi-ong <pugo9165@gmail.com>
2022-01-06 03:00:55 +0900

    "26.5test"

---
+++ .platform/hooks/prebuild/01_django_migrate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source "$PYTHONPATH/activate" && {
+# log which migrations have already been applied
+python manage.py showmigrations;
+# migrate
+python manage.py migrate --noinput;
+}
