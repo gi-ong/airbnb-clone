95e121252d52f2a895682add681b47fc45921349
gi-ong <pugo9165@gmail.com>
2022-01-06 22:44:26 +0900

    ""

--- .platform/hooks/postdeploy/01_migrations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source "/airbnb-clone-zLlG9Wid/bin/activate" && {
-# log which migrations have already been applied
-python manage.py showmigrations;
-# migrate
-python manage.py migrate --noinput;
-}
