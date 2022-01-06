ae60f571030b3ef81d8061cf29ecad584a768156
gi-ong <pugo9165@gmail.com>
2022-01-06 17:05:33 +0900

    "26.5 re~~~~"

--- .platform/hooks/predeploy/01_django_migrate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source "$PYTHONPATH/activate" && {
-# log which migrations have already been applied
-python manage.py showmigrations;
-# migrate
-python manage.py migrate --noinput;
-}
